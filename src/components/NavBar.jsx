80e92b0ad0fa84cf94f6c1c986b2613b141e6ec2
Rodrigo <rodrydanielmir@gmail.com>
2022-12-01 19:37:12 -0300

    a

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -3,7 +3,7 @@ import Nav from 'react-bootstrap/Nav';
 import Navbar from 'react-bootstrap/Navbar';
 import { Link } from "react-router-dom";
 import { useState } from 'react';
-import CartSideBar from './cartSideBar';
+import CartSideBar from "./CartSideBar"
 
 function NavBar() {
   const [show, setShow] = useState(false);
